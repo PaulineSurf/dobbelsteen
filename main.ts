baa44de2a6ec1c19c99ae61cca11cdb5960dd6a8
PaulineSurf <pauline.de.cuyper11@gmail.com>
2025-02-20 13:48:41 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("" + (randint(1, 6)))
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
